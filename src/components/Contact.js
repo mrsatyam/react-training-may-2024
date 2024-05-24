f74ba79689fae20f564ea8f1a18e741dddab35d0
Satyam Joshi <mrsatyamjoshi@gmail.com>
2024-05-24 14:14:30 +0530

    implemented useNavigate() hook

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Contact = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate('/');
+  };
+
   return (
     <div>
       <h1>Contact Page</h1>
       <p>Get in touch with us through Email: foodify@food.com </p>
+      <button onClick={handleNavigate}>Go to Home</button>
+
     </div>
   );
 };
